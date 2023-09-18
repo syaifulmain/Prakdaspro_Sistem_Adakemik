06a1a24cd8563afebc0992eb68ded3afe1f28a75
Muhamad Syaifullah <muhamadsyaifullah166333@gmail.com>
2023-09-18 13:17:16 +0700

    menambahkan MainApps

---
+++ src/MainApps.java
@@ -0,0 +1,42 @@
+public class MainApps {
+    public static void main(String[] args) {
+       
+    }
+    // TODO: login view
+
+    /* DASHBOARD MAHASISWA */
+        // GENERAL
+            // TODO: biodata -> menampikan biodata mahasiswa
+
+        
+            // AKADEMIK
+            // TODO: KRS -> menampilkan krs
+
+            // TODO: jadwal perkuliahan -> menampilkan jadwal perkuliahan
+
+            // TODO: nilai mahasiswa -> menampilkan mahasiswa
+
+            // TODO: LMS -> ???
+
+            // TODO: presensi -> menampilkan presensi
+
+            // TODO: data skpi -> menampilkan,menambah,menghapus data skpi
+
+            // UKT
+            // TODO: pembayaran ukt -> menampilkan ukt persemester
+
+            // TODO: keringanan ukt -> ????
+
+    /* DASHBOARD MAHASISWA */
+
+
+    /* DASHBOARD DOSEN */
+
+    /* DASHBOARD DOSEN */
+
+
+    /* DASHBOARD ADMIN */
+
+    /* DASHBOARD ADMIN */
+
+}
